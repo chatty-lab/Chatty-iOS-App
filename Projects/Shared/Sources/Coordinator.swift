b944570c1e6cd8a88bc4b9e186021b5b4556fba6
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-26 16:48:04 +0900

    Coordinator Protocol & Type 추가 [IOS-15]

---
+++ Projects/Shared/Sources/Coordinator.swift
@@ -0,0 +1,32 @@
+//
+//  Coordinator.swift
+//  Shared
+//
+//  Created by walkerhilla on 12/25/23.
+//
+
+import UIKit
+
+public protocol Coordinator: AnyObject {
+  var finishDelegate: CoordinatorFinishDelegate? { get set }
+  var navigationController: UINavigationController { get set }
+  var childCoordinators: [Coordinator] { get set }
+  var type: CoordinatorType { get set }
+  
+  init(_ navigationController: UINavigationController)
+  
+  func start()
+  func finish()
+}
+
+extension Coordinator {
+  public func finish() {
+    childCoordinators.removeAll()
+    finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+  }
+}
+
+public protocol CoordinatorFinishDelegate: AnyObject {
+  func coordinatorDidFinish(childCoordinator: Coordinator)
+}
+
---
+++ Projects/Shared/Sources/CoordinatorType.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorType.swift
+//  Shared
+//
+//  Created by walkerhilla on 12/25/23.
+//
+
+import UIKit
+
+public enum CoordinatorType {
+  case app, tab
+  case onboarding, live, chat, feed, myChatty
+}
