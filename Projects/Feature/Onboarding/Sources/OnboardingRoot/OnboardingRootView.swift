8bff54a98fded4d3afe9e4f9045eacfe091e1615
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-26 18:25:58 +0900

    refactor: Onboarding -> OnboardingRoot로 변경 [IOS-15]

---
+++ Projects/Feature/Onboarding/Sources/OnboardingRoot/OnboardingRootView.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingView.swift
+//  FeatureOnboarding
+//
+//  Created by walkerhilla on 12/26/23.
+//
+
+import UIKit
+import SnapKit
+import SharedThirdPartyLib
+
+final class OnboardingRootView: UIView {
+  
+}
