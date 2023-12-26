09970e32563c3798e331df32bdb21b32e6928387
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-26 18:36:02 +0900

    Info.plist Font 추가 [IOS-15]

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -28,6 +28,17 @@ let infoPlist: [String: Plist.Value] = [
         ]
       ]
     ]
+  ],
+  "UIAppFonts": [
+    "Pretendard-Black.ttf",
+    "Pretendard-Bold.ttf",
+    "Pretendard-ExtraBold.ttf",
+    "Pretendard-ExtraLight.ttf",
+    "Pretendard-Light.ttf",
+    "Pretendard-Medium.ttf",
+    "Pretendard-Regular.ttf",
+    "Pretendard-SemiBold.ttf",
+    "Pretendard-Thin.ttf"
   ]
 ]
 
