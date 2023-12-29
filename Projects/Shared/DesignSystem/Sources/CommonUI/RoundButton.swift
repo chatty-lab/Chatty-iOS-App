d189f53af33ae0c3e61201f2ce9720a04b796023
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-29 15:55:09 +0900

    refactor: Touchable, StateConfigurable 준수
    Touchable: 터치 가능한 뷰 객체
    
    StateConfigurable: 상태를 지닌 뷰 객체 [IOS-15]

--- Projects/Shared/DesignSystem/Sources/CommonUI/RoundButton.swift
+++ Projects/Shared/DesignSystem/Sources/CommonUI/RoundButton.swift
@@ -7,47 +7,79 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 import SnapKit
 
-public protocol RoundButtonDelegate: AnyObject {
-  func didTapped()
-}
-
-public class RoundButton: UIButton {
+open class RoundButton: UIButton, Touchable {
+  private let text: String
+  private var customConfiguration = UIButton.Configuration.filled()
+  private let disposeBag = DisposeBag()
+  public let didTouch: RxRelay.PublishRelay<Void> = .init()
   
-  let text: String
-  var customConfiguration = UIButton.Configuration.filled()
-  public weak var delegate: RoundButtonDelegate?
+  public var currentState: State? {
+    didSet {
+      updateForCurrentState()
+    }
+  }
+  
+  public var configurations: [State : Configuration] = [:]
   
   public init(text: String) {
     self.text = text
     super.init(frame: .zero)
     
+    bind()
     configureUI()
-    addTarget(self, action: #selector(didTapped), for: .touchUpInside)
   }
   
-  required init?(coder: NSCoder) {
+  @available(*, unavailable)
+  required public init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 }
 
 extension RoundButton {
+  private func bind() {
+    self.rx.tap
+      .map { _ in Void() }
+      .bind(to: didTouch)
+      .disposed(by: disposeBag)
+  }
+  
   private func configureUI() {
     var titleAttr = AttributedString(stringLiteral: text)
     titleAttr.font = Font.Pretendard(.SemiBold).of(size: 16)
+    titleAttr.foregroundColor = UIColor(asset: Colors.basicWhite)
     
     customConfiguration.attributedTitle = titleAttr
     customConfiguration.titleAlignment = .center
-    customConfiguration.baseBackgroundColor = UIColor(asset: Colors.gray300)
-    customConfiguration.baseForegroundColor = UIColor(asset: Colors.basicWhite)
     
     configuration = customConfiguration
     
     layer.cornerRadius = 6
   }
+}
+
+extension RoundButton: StateConfigurable {
+  public enum State {
+    case enabled
+    case disabled
+  }
   
-  @objc func didTapped() {
-    delegate?.didTapped()
+  public struct Configuration {
+    let backgroundColor: UIColor
+    let isEnabled: Bool
+    
+    public init(backgroundColor: UIColor, isEnabled: Bool) {
+      self.backgroundColor = backgroundColor
+      self.isEnabled = isEnabled
+    }
+  }
+  
+  public func updateForCurrentState() {
+    guard let currentState,
+          let config = configurations[currentState] else { return }
+    configuration?.background.backgroundColor = config.backgroundColor
+    isEnabled = config.isEnabled
   }
 }
