9b3a448542cc77ebc0f07e7c7a46716758d6d412
HUNHIE LEE <huny98.dev@gmail.com>
2024-01-11 07:55:10 +0900

    Add: 공통 프로토콜 [IOS-15]

---
+++ Projects/Shared/DesignSystem/Sources/CommonUI/Protocol/Fadeable.swift
@@ -0,0 +1,82 @@
+//
+//  Fadeable.swift
+//  SharedDesignSystem
+//
+//  Created by walkerhilla on 1/3/24.
+//
+
+import UIKit
+
+public protocol Fadeable: UIView {
+  func fadeIn(_ view: UIView, duration: Duration, alpha: Alpha, completion: (() -> Void)?)
+  func fadeOut(_ view: UIView, duration: Duration, alpha: Alpha, completion: (() -> Void)?)
+  func flashAlpha(lowAlpha: CGFloat, highAlpha: CGFloat, duration: Duration, completion: (() -> Void)?)
+}
+
+public extension Fadeable {
+  func fadeIn(_ view: UIView, duration: Duration = .normal, alpha: Alpha, completion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: duration.value) {
+      view.alpha = alpha.value
+    } completion: { _ in
+      completion?()
+    }
+    
+  }
+  
+  func fadeOut(_ view: UIView, duration: Duration = .normal, alpha: Alpha, completion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: duration.value) {
+      view.alpha = alpha.value
+    } completion: { _ in
+      completion?()
+    }
+  }
+  
+  func flashAlpha(lowAlpha: CGFloat, highAlpha: CGFloat, duration: Duration, completion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: duration.value / 2, animations: {
+      self.alpha = lowAlpha
+    }) { _ in
+      UIView.animate(withDuration: duration.value / 2, animations: {
+        self.alpha = highAlpha
+      }, completion: { _ in
+        completion?()
+      })
+    }
+  }
+}
+
+public enum Alpha {
+  case full
+  case transparent
+  case custom(value: CGFloat)
+  
+  var value: CGFloat {
+    switch self {
+    case .full:
+      return 1
+    case .transparent:
+      return 0
+    case .custom(let value):
+      return value
+    }
+  }
+}
+
+public enum Duration {
+  case fast
+  case normal
+  case slow
+  case custom(TimeInterval)
+  
+  var value: TimeInterval {
+    switch self {
+    case .fast:
+      return 0.12
+    case .normal:
+      return 0.25
+    case .slow:
+      return 0.3
+    case .custom(let timeInterval):
+      return timeInterval
+    }
+  }
+}
--- Projects/Shared/DesignSystem/Sources/CommonUI/Protocol/Highlightable.swift
+++ Projects/Shared/DesignSystem/Sources/CommonUI/Protocol/Highlightable.swift
@@ -1,5 +1,5 @@
 //
-//  TouchableHighlight.swift
+//  Highlightable.swift
 //  SharedDesignSystem
 //
 //  Created by walkerhilla on 12/30/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// `TouchableHighlight` 프로토콜은 UIControl 또는 그 서브 클래스에서 터치 이벤트에 대응하여 투명도를 변경하는 애니메이션을 제공해요.
+/// `Highlightable` 프로토콜은 UIView 또는 그 서브 클래스에서 투명도를 변경하는 애니메이션을 제공해요.
 ///
 /// 이 프로토콜을 준수하는 객체는 `highlight`와 `unhighlight` 메소드를 사용하여 투명도 변화를 구현해요.
 ///
@@ -16,12 +16,12 @@ import UIKit
 /// - `highlight`: 뷰의 투명도를 줄여 하이라이트 효과를 적용해요.
 /// - `unhighlight`: 뷰의 투명도를 원래대로 되돌려요.
 ///
-public protocol TouchableHighlight: UIControl {
+public protocol Highlightable: UIView {
   func highlight(_ view: UIView)
   func unhighlight(_ view: UIView)
 }
 
-public extension TouchableHighlight {
+public extension Highlightable {
   func highlight(_ view: UIView) {
     UIView.animate(withDuration: 0.1, animations: {
       view.alpha = 0.65
