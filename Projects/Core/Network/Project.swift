15270624e5e6942f847b2bd3263cd949feebf83f
HUNHIE LEE <huny98.dev@gmail.com>
2024-01-15 23:24:06 +0900

    fix: Info.plist
    api baseURL from xcconfig [IOS-23]

--- Projects/Core/Network/Project.swift
+++ Projects/Core/Network/Project.swift
@@ -1,6 +1,17 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by walkerhilla on 12/12/23.
+//
+
 import ProjectDescription
 import ProjectDescriptionHelpers
 import DependencyPlugin
+
+let infoPlist: [String: Plist.Value] = [
+  "baseURL": "$(baseURL)",
+]
 
 let project = Project.makeModule(
   name: ModulePath.Core.name+ModulePath.Core.Network.rawValue,
@@ -16,6 +27,7 @@ let project = Project.makeModule(
     .core(
       implements: .Network,
       factory: .init(
+        infoPlist: .extendingDefault(with: infoPlist),
         dependencies: [
           .core(interface: .Network)
         ]
