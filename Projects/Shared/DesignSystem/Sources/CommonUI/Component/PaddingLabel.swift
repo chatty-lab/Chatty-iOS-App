6c432601da6d5b6017f666d8f1c90d51182d37ac
yoonjiho37 <yoonjiho37@github.com>
2024-01-25 02:31:20 +0900

    fix: Padding Label 모듈이동에 따른 접근제어자 수정 [IOS-37]

--- Projects/Shared/DesignSystem/Sources/CommonUI/Component/PaddingLabel.swift
+++ Projects/Shared/DesignSystem/Sources/CommonUI/Component/PaddingLabel.swift
@@ -10,7 +10,7 @@ import UIKit
 public final class BasePaddingLabel: UILabel {
   private var padding = UIEdgeInsets(top: 12.0, left: 16.0, bottom: 12.0, right: 16.0)
   
-  convenience init(padding: UIEdgeInsets) {
+  public convenience init(padding: UIEdgeInsets) {
     self.init()
     self.padding = padding
   }
