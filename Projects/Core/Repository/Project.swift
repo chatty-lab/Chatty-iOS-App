5df10de54cf2edaf94bf713faae812d207783d26
yoonjiho37 <yoonjiho37@github.com>
2024-01-29 11:17:52 +0900

    chore: CoreRepository 모듈 추가 [IOS-35]

--- Projects/Core/Repository/Project.swift
+++ Projects/Core/Repository/Project.swift
@@ -6,38 +6,13 @@ let project = Project.makeModule(
   name: ModulePath.Core.name+ModulePath.Core.Repository.rawValue,
   targets: [
     .core(
-      interface: .Repository,
+      implements: .Repository,
       factory: .init(
         dependencies: [
           .core(implements: .Network),
           .core(implements: .Storage),
-          .shared
         ]
       )
-    ),
-    .core(
-      implements: .Repository,
-      factory: .init(
-        dependencies: [
-          .core(interface: .Repository)
-        ]
-      )
-    ),
-    .core(
-      testing: .Repository,
-      factory: .init(
-        dependencies: [
-          .core(interface: .Repository)
-        ]
-      )
-    ),
-    .core(
-      tests: .Repository,
-      factory: .init(
-        dependencies: [
-          .core(testing: .Repository)
-        ]
-      )
-    ),
+    )
   ]
 )
